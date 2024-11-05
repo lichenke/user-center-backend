7ea79f058557c3e8f3367aa04142ecb61b61e744
lichenke <llw04758@gmail.com>
2024-11-05 23:41:59 +0800

    first commit

---
+++ src/main/java/xyz/cafebabe/ucbackend/UcBackendApplication.java
@@ -0,0 +1,13 @@
+package xyz.cafebabe.ucbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UcBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UcBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/xyz/cafebabe/ucbackend/UcBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.cafebabe.ucbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UcBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
