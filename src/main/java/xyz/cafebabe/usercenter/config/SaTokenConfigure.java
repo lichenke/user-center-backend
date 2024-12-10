a70ab25ebc5f4d13e7619b4cbcf47d950c5a23fc
lichenke <llw04758@gmail.com>
2024-12-10 16:12:47 +0800

    注册Sa-Token拦截器，打开注解式鉴权功能

---
+++ src/main/java/xyz/cafebabe/usercenter/config/SaTokenConfigure.java
@@ -0,0 +1,17 @@
+package xyz.cafebabe.usercenter.config;
+
+import cn.dev33.satoken.interceptor.SaInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+// 注册 Sa-Token 拦截器，打开注解式鉴权功能
+
+@Configuration
+public class SaTokenConfigure implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new SaInterceptor()).addPathPatterns("/**");
+    }
+}
